105f98db62053670b29eb824f3778c3c98840cd1
codewithdevu <divyanshyadav8302@gmail.com>
2026-09-04 21:26:54 +0530

    day 1 in dsa in js

---
+++ app.js
@@ -0,0 +1,25 @@
+// let a = 10;
+// let b = 20;
+
+// console.log("The sum of a and b is: " + (a + b));
+
+// let a = 12;
+// let b = "13";
+
+// console.log("The sum of a and b is: " + (a + b));
+// console.log(typeof (a + b));
+
+// 1 + "2"   is not addition its concatenation because 1 is number and 2 is string so it will convert number to string and then concatenate it.
+
+// let a = 10;
+// let b = 20;
+
+// console.log("sum of 10 and 20 is " + (a + b));  
+
+// console.log("1" + 2); // addition when two numbers are added it will give sum but when one number is string and other is number then it will convert number to string and then concatenate it. so the output will be 12 not 3. this is callled type coercion.
+
+// - , * , / , % these operators are used for subtraction, multiplication, division and modulus respectively. when we use these operators with string and number then it will convert string to number and then perform the operation. if the string is not a number then it will return NaN (Not a Number).
+
+// console.log ("me" - 1); // NaN
+
+
