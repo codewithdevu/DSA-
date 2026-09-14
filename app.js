6d1112ff6847930dc12753463a9a3e4e271eb4dc
codewithdevu <divyanshyadav8302@gmail.com>
2026-09-15 01:22:44 +0530

    day 9  of learning DSA in JS

--- app.js
+++ app.js
@@ -750,4 +750,31 @@
 // *       *
 
 
-//  
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter an odd number: "));
+
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//         if (i === j || i + j === n - 1) {
+//             process.stdout.write("* ");
+//         } else {
+//             process.stdout.write("  ");
+//         }
+//     }
+//     console.log();
+// }
+
+//
+
+// let prompt = require("prompt-sync")();
+// let n = Number(prompt("Enter an odd number: "));
+
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n - i; j++) {
+//             process.stdout.write(" ");
+//     }
+//     for(let k = 1; k<=i; k++){
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+// }
