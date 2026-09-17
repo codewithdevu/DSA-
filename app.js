52e6f0ced728c213d699bbedf4962e55a1f1d18a
codewithdevu <divyanshyadav8302@gmail.com>
2026-09-17 14:27:37 +0530

    day 11  of learning DSA in JS

--- app.js
+++ app.js
@@ -796,7 +796,7 @@
 // let n = Number(prompt("Enter a number: "));
 
 // for (let i = 1; i <= n; i++) {
-//     for (let j = 1; j <= n * 2; j++) {
+//     for (let j = 1; j <= 2*n-1; j++) {
 //         if (i === j || i+j == n*2) {
 //             process.stdout.write("* ");
 //         } else {
